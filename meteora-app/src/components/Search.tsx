ee40179a03437e541377a00fd24324d6df78e33c
gabryel <valvanogabryel@gmail.com>
2023-06-19 19:27:52 -0300

    font-fixed

--- meteora-app/src/components/Search.tsx
+++ meteora-app/src/components/Search.tsx
@@ -4,7 +4,7 @@ export function Search() {
   return (
     <div>
       <form
-        className="flex gap-2 w-full px-4 py-8 justify-center md:gap-4 md:p-0 md:pr-6 flex-wrap"
+        className="flex gap-2 w-full px-4 py-8 justify-center md:gap-4 md:p-0 md:pr-6"
         onSubmit={(e) => {
           e.preventDefault();
           console.log("pesquisando...");
@@ -13,7 +13,7 @@ export function Search() {
         <input
           type="search"
           placeholder="Digite o produto"
-          className="border border-black pl-4 focus:outline-none placeholder:text-meteora-dark-gray md:w-36 md:placeholder:text-xs+1 lg:placeholder:text-[15px] lg:w-44"
+          className="text-xs+1 border border-black pl-4 focus:outline-none placeholder:text-meteora-dark-gray md:w-36 lg:placeholder:text-[15px] lg:w-44"
         />
         <button className="border border-black font-medium px-4 py-2 md:text-white md:border-white md:text-xs+1 lg:text-base">
           Search
