34a7f8ac170d30fef2f3e1062fdcf1af3edbd369
gabryel <valvanogabryel@gmail.com>
2023-06-19 19:36:06 -0300

    ;

--- meteora-app/src/components/MenuHamburguer/MenuBurguer.tsx
+++ meteora-app/src/components/MenuHamburguer/MenuBurguer.tsx
@@ -8,7 +8,7 @@ import { Navbar } from "./Navbar";
 export default function MenuBurguer() {
   const [isOpen, setIsOpen] = useState(false);
 
-  if (isOpen) {
+  if (typeof document !== "undefined" && isOpen) {
     document.body.classList.add("overflow-hidden");
   } else {
     document.body.classList.remove("overflow-hidden");
