c90c8ec36e5dc36f9c910fd3ccf47e3cb5696d02
gabryel <valvanogabryel@gmail.com>
2023-06-20 18:53:09 -0300

    Update Banner.tsx

--- meteora-app/src/components/Banner.tsx
+++ meteora-app/src/components/Banner.tsx
@@ -5,15 +5,13 @@ import bannerDesktop from "@/assets/desktop/Banner carousel 1 _ 1440 (1).png";
 
 export function Banner() {
   return (
-    <div className="min-w-screen">
-      <a href="/" className="min-w-screen">
-        {/* Mobile */}
-        <Image src={bannerMobile} alt="" className="md:hidden m-auto" />
-        {/* Tablet */}
-        <Image src={bannerTablet} alt="" className="hidden md:flex lg:hidden" />
-        {/* Desktop */}
-        <Image src={bannerDesktop} alt="" className="hidden lg:flex" />
-      </a>
+    <div className="min-w-screen bg-[#8960B4]">
+      {/* Mobile */}
+      <Image src={bannerMobile} alt="" className="md:hidden m-auto" />
+      {/* Tablet */}
+      <Image src={bannerTablet} alt="" className="hidden md:flex lg:hidden" />
+      {/* Desktop */}
+      <Image src={bannerDesktop} alt="" className="hidden lg:flex" />
     </div>
   );
 }
