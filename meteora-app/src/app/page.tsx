043a74b92e16b5793db37201b05168ae013ffb9b
gabryel <valvanogabryel@gmail.com>
2023-06-20 18:50:25 -0300

    ;

--- meteora-app/src/app/page.tsx
+++ meteora-app/src/app/page.tsx
@@ -1,10 +1,11 @@
 import { Header } from "@/components/Header";
+import { Banner } from "@/components/Banner";
 
 export default function Home() {
   return (
     <>
       <Header />
-      {/* <Banner/> */}
+      <Banner />
     </>
   );
 }
---
+++ meteora-app/src/components/Banner.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import bannerMobile from "@/assets/mobile/Banner carousel 1 _ 375.png";
+import bannerTablet from "@/assets/tablet/Banner carousel 1 _ 768.png";
+import bannerDesktop from "@/assets/desktop/Banner carousel 1 _ 1440 (1).png";
+
+export function Banner() {
+  return (
+    <div className="min-w-screen">
+      <a href="/" className="min-w-screen">
+        {/* Mobile */}
+        <Image src={bannerMobile} alt="" className="md:hidden m-auto" />
+        {/* Tablet */}
+        <Image src={bannerTablet} alt="" className="hidden md:flex lg:hidden" />
+        {/* Desktop */}
+        <Image src={bannerDesktop} alt="" className="hidden lg:flex" />
+      </a>
+    </div>
+  );
+}
