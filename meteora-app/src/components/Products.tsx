5bd00cc53abba417456404666d8a914710538bb8
gabryel <valvanogabryel@gmail.com>
2023-06-21 10:51:16 -0300

    /

--- meteora-app/src/components/Products.tsx
+++ meteora-app/src/components/Products.tsx
@@ -7,21 +7,23 @@ export function Products() {
   return (
     <section>
       <SectionTitle>Produtos que estão bombando!</SectionTitle>
+      {/* card's grid */}
+      <div className="flex flex-col items-center sm:grid sm:grid-cols-2 m-auto">
+        {/* card */}
+        <div className="m-8 border max-w-xs">
+          <div className="">
+            <Image src={productImage} alt="placeholder" className="" />
+          </div>
 
-      {/* card */}
-      <div className="m-8 border">
-        <div className="">
-          <Image src={productImage} alt="placeholder" className="" />
-        </div>
-
-        <div className="flex flex-col justify-start text-meteora-dark p-4">
-          <h5 className="font-bold -mt-1 mb-4">Camiseta Conforto</h5>
-          <p className="text-xs+1 mb-4">
-            Multicores e tamanhos. Tecido de algodão 100%, fresquinho para o
-            verão. Modelagem unissex.
-          </p>
-          <span className="font-bold mb-3">R$ 70,00</span>
-          <PurpleButton width="40">Ver mais</PurpleButton>
+          <div className="flex flex-col justify-start text-meteora-dark p-4">
+            <h5 className="font-bold -mt-1 mb-4">Camiseta Conforto</h5>
+            <p className="text-xs+1 mb-4">
+              Multicores e tamanhos. Tecido de algodão 100%, fresquinho para o
+              verão. Modelagem unissex.
+            </p>
+            <span className="font-bold mb-3">R$ 70,00</span>
+            <PurpleButton width="40">Ver mais</PurpleButton>
+          </div>
         </div>
       </div>
     </section>
