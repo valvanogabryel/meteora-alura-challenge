f887231076a24f3b94f43c5b335f5ef72fa4aca1
gabryel <valvanogabryel@gmail.com>
2023-06-21 11:22:16 -0300

    ;

--- meteora-app/src/components/Products.tsx
+++ meteora-app/src/components/Products.tsx
@@ -5,10 +5,10 @@ import productImage from "@/assets/mobile/Imagens cards/Card camiseta.png";
 
 export function Products() {
   return (
-    <section>
+    <section className="mt-10">
       <SectionTitle>Produtos que estão bombando!</SectionTitle>
       {/* card's grid */}
-      <div className="flex flex-col items-center sm:grid sm:grid-cols-2 m-auto">
+      <div className="flex flex-col items-center -mt-2 sm:grid sm:grid-cols-2 lg:grid-cols-3">
         {/* card */}
         <div className="m-8 border max-w-xs">
           <div className="">
@@ -22,7 +22,52 @@ export function Products() {
               verão. Modelagem unissex.
             </p>
             <span className="font-bold mb-3">R$ 70,00</span>
-            <PurpleButton width="40">Ver mais</PurpleButton>
+            <PurpleButton width={40}>Ver mais</PurpleButton>
+          </div>
+        </div>
+        <div className="m-8 border max-w-xs">
+          <div className="">
+            <Image src={productImage} alt="placeholder" className="" />
+          </div>
+
+          <div className="flex flex-col justify-start text-meteora-dark p-4">
+            <h5 className="font-bold -mt-1 mb-4">Camiseta Conforto</h5>
+            <p className="text-xs+1 mb-4">
+              Multicores e tamanhos. Tecido de algodão 100%, fresquinho para o
+              verão. Modelagem unissex.
+            </p>
+            <span className="font-bold mb-3">R$ 70,00</span>
+            <PurpleButton width={40}>Ver mais</PurpleButton>
+          </div>
+        </div>
+        <div className="m-8 border max-w-xs">
+          <div className="">
+            <Image src={productImage} alt="placeholder" className="" />
+          </div>
+
+          <div className="flex flex-col justify-start text-meteora-dark p-4">
+            <h5 className="font-bold -mt-1 mb-4">Camiseta Conforto</h5>
+            <p className="text-xs+1 mb-4">
+              Multicores e tamanhos. Tecido de algodão 100%, fresquinho para o
+              verão. Modelagem unissex.
+            </p>
+            <span className="font-bold mb-3">R$ 70,00</span>
+            <PurpleButton width={40}>Ver mais</PurpleButton>
+          </div>
+        </div>
+        <div className="m-8 border max-w-xs">
+          <div className="">
+            <Image src={productImage} alt="placeholder" className="" />
+          </div>
+
+          <div className="flex flex-col justify-start text-meteora-dark p-4">
+            <h5 className="font-bold -mt-1 mb-4">Camiseta Conforto</h5>
+            <p className="text-xs+1 mb-4">
+              Multicores e tamanhos. Tecido de algodão 100%, fresquinho para o
+              verão. Modelagem unissex.
+            </p>
+            <span className="font-bold mb-3">R$ 70,00</span>
+            <PurpleButton width={40}>Ver mais</PurpleButton>
           </div>
         </div>
       </div>
--- meteora-app/src/components/PurpleButton.tsx
+++ meteora-app/src/components/PurpleButton.tsx
@@ -5,14 +5,14 @@ export function PurpleButton({
   width,
 }: {
   children: ReactNode;
-  width?: string;
+  width?: number;
 }) {
   const widthValue = width;
 
   return (
     <button
-      className={`bg-meteora-light-purple text-white font-medium py-2 pr-4 pl-[18px] col-span-3 ${
-        width && `w-[${widthValue}%]`
+      className={`bg-meteora-light-purple text-white font-medium py-2 transition-all duration-300 pr-4 pl-[18px] col-span-3 hover:bg-[#8960B4] ${
+        widthValue && `w-[${widthValue}%]`
       }`}
     >
       {children}
