d461516c2b55d3884ce439b71f93fd1c405bc971
gabryel <valvanogabryel@gmail.com>
2023-06-21 11:50:11 -0300

    ;

--- meteora-app/src/hooks/useCategories.ts
+++ meteora-app/src/hooks/useCategories.ts
@@ -2,27 +2,27 @@ const useCategories = (size: "mobile" | "tablet" | "desktop") => {
   const categories = [
     {
       name: "Camisetas",
-      image: `/assets/categorias/${size}/Categoria camiseta.png`,
+      image: `/assets/categorias/${size}/camisetas.png`,
     },
     {
       name: "Bolsas",
-      image: `/assets/categorias/${size}/Categoria Bolsas.png`,
+      image: `/assets/categorias/${size}/bolsas.png`,
     },
     {
       name: "Calçados",
-      image: `/assets/categorias/${size}/Categoria Calçados.png`,
+      image: `/assets/categorias/${size}/calcados.png`,
     },
     {
       name: "Calças",
-      image: `/assets/categorias/${size}/Categoria calças.png`,
+      image: `/assets/categorias/${size}/calcas.png`,
     },
     {
       name: "Casacos",
-      image: `/assets/categorias/${size}/Categoria casacos.png`,
+      image: `/assets/categorias/${size}/casacos.png`,
     },
     {
       name: "Óculos",
-      image: `/assets/categorias/${size}/Categoria óculos.png`,
+      image: `/assets/categorias/${size}/oculos.png`,
     },
   ];
 
