4bf4a0a2151c708a7a46f7b8f65357e728bf974e
gabryel <valvanogabryel@gmail.com>
2023-06-22 17:30:56 -0300

    Update CategoryCards.tsx

--- meteora-app/src/components/Categories/CategoryCards.tsx
+++ meteora-app/src/components/Categories/CategoryCards.tsx
@@ -6,7 +6,7 @@ export function CategoryCards() {
   const categories = useCategoriesImage();
 
   return (
-    <div className="grid grid-cols-2 gap-8 mt-8 w-[86%] sm:grid-cols-3 md:w-[90%] lg:flex lg:justify-center">
+    <div className="grid grid-cols-2 gap-8 mt-8 w-[86%] sm:grid-cols-3 md:w-[90%] lg:flex lg:justify-center select-none">
       {categories.map((category) => (
         <CategoryCard category={category} key={category.name} />
       ))}
