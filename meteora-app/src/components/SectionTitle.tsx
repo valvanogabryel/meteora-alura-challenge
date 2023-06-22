40102c14d66928c4ebdd831085c247f1ec357fae
gabryel <valvanogabryel@gmail.com>
2023-06-22 18:02:43 -0300

    ;

--- meteora-app/src/components/SectionTitle.tsx
+++ meteora-app/src/components/SectionTitle.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export function SectionTitle({ children }: { children: ReactNode }) {
   return (
-    <h3 className="font-medium text-3xl text-meteora-dark text-center mb-2 lg:text-[32px] lg:mt-20">
+    <h3 className="select-none font-medium text-3xl text-meteora-dark text-center mb-2 lg:text-[32px] lg:mt-20">
       {children}
     </h3>
   );
